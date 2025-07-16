2d28914c9b5c883eef1a406051d1b03b69ccefbe
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-07-16 17:51:59 +0300

    Add get user details call to inventory service

--- src/main/java/com/example/report_service/util/message/InfoMessages.java
+++ src/main/java/com/example/report_service/util/message/InfoMessages.java
@@ -2,4 +2,5 @@ package com.example.report_service.util.message;
 
 public class InfoMessages {
     public final static String CONSUMING_MESSAGE = "Consuming message with key = {}, value = {}";
+    public final static String CALLING_INVENTORY_SERVICE = "Calling inventory service to get user details for user with id {}";
 }
