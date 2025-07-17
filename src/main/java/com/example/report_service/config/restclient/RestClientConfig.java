11fc28b47270add8f64f76195523722d1514dae2
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-07-17 08:17:21 +0300

    Minor improvements

--- src/main/java/com/example/report_service/config/restclient/RestClientConfig.java
+++ src/main/java/com/example/report_service/config/restclient/RestClientConfig.java
@@ -1,15 +1,20 @@
 package com.example.report_service.config.restclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
+    @Value("${inventory.service.base.url}")
+    private String baseUrl;
+
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
         return builder
-                .baseUrl("http://inventory-service:8080")
+                .baseUrl(baseUrl)
                 .build();
     }
 }
